df0dc2c8502deca93634f2850f51432954cf967b
eslack <ericslack31@gmail.com>
2024-10-29 13:16:31 -0500

    created report for num or words and each letter

--- main.py
+++ main.py
@@ -1,4 +1,27 @@
+def sort_on(tup):
+    return tup[1]
+
 with open("books/frankenstein.txt") as f:
+    letter={}
+    ordered = []
     file_contents = f.read()
-    words = file_contents.split()
-    print (len(words))
+    wordcount = file_contents.split()
+    file_contentsL = file_contents.lower()
+    for char in file_contentsL:
+        if char.isalpha():
+            if char in letter:
+                letter[char] += 1
+            else:
+                letter[char] = 1
+    
+    ordered = list(letter.items())
+    ordered.sort(reverse= True, key = sort_on)
+    
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{len(wordcount)} words found in the document")
+    print()
+
+    for char in range(0,len(ordered)):
+        print(f"The '{ordered[char][0]}' character was found {ordered[char][1]} times")
+    
+    print("--- End report ---")
